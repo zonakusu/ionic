6a7457ffc4fedd560d8e0b452819c418542ec92c
Max Lynch <max@drifty.com>
2014-08-22 18:55:31 -0500

    Anim progress

--- js/angular/service/animations.js
+++ js/angular/service/animations.js
@@ -8,21 +8,45 @@
   In (reverse of push out)
 */
 IonicModule.animation('.ios-transition', function() {
+  var inAnimator = collide.animation({
+    easing: 'cubic-bezier(0.4, 0.6, 0.2, 1)',
+    duration: 400
+  });
+
+  var outAnimator = collide.animation({
+    easing: 'cubic-bezier(0.4, 0.6, 0.2, 1)',
+    duration: 400
+  });
 
   return {
     enter: function(element, done) {
-      var inAnimator = collide.animation({
-        easing: 'cubic-bezier(0.4, 0.6, 0.2, 1)',
-        duration: 400
-      });
-      inAnimator.on('step', function(v) {
-        element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + (100 - (v*100)) + '%, 0, 0)';
-      }).on('complete', function() {
+      var isReverse = element.hasClass('reverse');
+
+      inAnimator
+      .on('start', function(v) {
+        if(isReverse) {
+          element[0].zIndex = 0;
+        } else {
+          element[0].zIndex = 1;
+        }
+      })
+      .on('step', function(v) {
+        if(isReverse) {
+          // Slide in from -20% to 0
+          element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + Math.max(0, (-20 + (v*100))) + '%, 0, 0)';
+        } else {
+          // Slide in from 100% to 0
+          element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + (100 - (v*100)) + '%, 0, 0)';
+        }
+      })
+      .on('complete', function() {
         element[0].style.webkitTransform = element[0].style.transform = '';
+        element[0].style.zIndex = '';
         done();
       });
 
-      if(element.hasClass('reverse')) {
+      /*
+      if(isReverse) {
         inAnimator.reverse(true);
         inAnimator.percent(0);
         inAnimator.start();
@@ -31,19 +55,36 @@ IonicModule.animation('.ios-transition', function() {
         inAnimator.percent(0);
         inAnimator.start();
       }
+      */
+      inAnimator.percent(0);
+      inAnimator.start();
     },
     leave: function(element, done) {
-      var outAnimator = collide.animation({
-        easing: 'cubic-bezier(0.4, 0.6, 0.2, 1)',
-        duration: 400
-      });
-      outAnimator.on('step', function(v) {
-        element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + (100 + (v*100)) + '%, 0, 0)';
-      }).on('complete', function() {
+      var isReverse = element.hasClass('reverse');
+
+      outAnimator
+      .on('start', function(v) {
+        if(isReverse) {
+          element[0].zIndex = 1;
+        } else {
+          element[0].zIndex = 0;
+        }
+      })
+      .on('step', function(v) {
+        if(isReverse) {
+          // Slide from 0 to 100% (off screen)
+          element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + v*100 + '%, 0, 0)';
+        } else {
+          // Slide from 0 to -20%
+          element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + Math.max(-20, -v*100) + '%, 0, 0)';
+        }
+      })
+      .on('complete', function() {
         element[0].style.webkitTransform = element[0].style.transform = '';
         done();
       });
 
+      /*
       if(element.hasClass('reverse')) {
         outAnimator.reverse(true);
         outAnimator.percent(0);
@@ -53,6 +94,9 @@ IonicModule.animation('.ios-transition', function() {
         outAnimator.percent(0);
         outAnimator.start();
       }
+      */
+      outAnimator.percent(0);
+      outAnimator.start();
     },
     step: function(element, percent, done) {
     }
