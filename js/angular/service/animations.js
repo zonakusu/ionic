9bb6c48a3b2ed63518806f9625f356a19a4e5a1c
Max Lynch <max@drifty.com>
2014-08-22 18:42:10 -0500

    Collide animation

--- js/angular/service/animations.js
+++ js/angular/service/animations.js
@@ -1,31 +1,58 @@
+/**
+  Push:
+  In
+  Out
+
+  Pop:
+  Out (reverse of push in)
+  In (reverse of push out)
+*/
 IonicModule.animation('.ios-transition', function() {
-  var animator = collide.animation({
-    easing: 'cubic-bezier(0.4, 0.6, 0.2, 1)',
-      duration: 400
-  })
 
   return {
     enter: function(element, done) {
-      animator.percent(0);
-      animator.on('step', function(v) {
-        console.log('Stepping');
-        element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + (-100 + (v*100)) + '%, 0, 0)';
+      var inAnimator = collide.animation({
+        easing: 'cubic-bezier(0.4, 0.6, 0.2, 1)',
+        duration: 400
+      });
+      inAnimator.on('step', function(v) {
+        element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + (100 - (v*100)) + '%, 0, 0)';
       }).on('complete', function() {
         element[0].style.webkitTransform = element[0].style.transform = '';
         done();
       });
-      animator.start();
+
+      if(element.hasClass('reverse')) {
+        inAnimator.reverse(true);
+        inAnimator.percent(0);
+        inAnimator.start();
+      } else {
+        inAnimator.reverse(false);
+        inAnimator.percent(0);
+        inAnimator.start();
+      }
     },
     leave: function(element, done) {
-      animator.percent(0);
-      animator.on('step', function(v) {
-        console.log('Stepping BACK');
+      var outAnimator = collide.animation({
+        easing: 'cubic-bezier(0.4, 0.6, 0.2, 1)',
+        duration: 400
+      });
+      outAnimator.on('step', function(v) {
         element[0].style.webkitTransform = element[0].style.transform = 'translate3d(' + (100 + (v*100)) + '%, 0, 0)';
       }).on('complete', function() {
         element[0].style.webkitTransform = element[0].style.transform = '';
         done();
       });
-      animator.start();
+
+      if(element.hasClass('reverse')) {
+        outAnimator.reverse(true);
+        outAnimator.percent(0);
+        outAnimator.start();
+      } else {
+        outAnimator.reverse(false);
+        outAnimator.percent(0);
+        outAnimator.start();
+      }
     },
     step: function(element, percent, done) {
     }
