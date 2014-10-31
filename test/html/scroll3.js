afdcff570af496cfe9ef96997efb141298540efa
Perry Govier <govierp@gmail.com>
2014-10-31 13:28:34 -0500

    trying a new approach

--- test/html/scroll3.js
+++ test/html/scroll3.js
@@ -1,47 +1,40 @@
 /**
  * Created by perry on 10/28/14.
  */
-function initScrollPolyfill(element){
-  var isScrolling = false;
-  var lastScrollPos;
-  //var scrollParent = ionic.DomUtil.getParentOrSelfWithClass(element,'scroller', 10);
-  var scrollParent = document.getElementById('scroller'); // TODO: use domUtil to look for 'scroll' class
-  console.log(scrollParent);
-  var activePolyfillEnabled = true; // TODO: make this configurable
-  var eventObj = {
-    target:element,
-    scrollParent:scrollParent
-  };
-  ionic.on('scroll', startScroll, element);
-  ionic.on('touchmove', startScroll, element);
-  var debounceScrollEvents = ionic.Utils.debounce(loopScrollEvent, 100, false);
+var now = window.performance ?
+  angular.bind(window.performance, window.performance.now) :
+  Date.now;
 
-  function startScroll(){
-    //console.log('scroll detected', isScrolling,activePolyfillEnabled);
-    if((!isScrolling) && activePolyfillEnabled){
-      isScrolling = true;
-      debounceScrollEvents();
-      ionic.trigger('$scrollStart',eventObj, eventBubbles)
-    }
-    ionic.trigger('$scroll',{target:element}, eventBubbles);
-    lastScrollingPos = scrollParent.scrollTop;
-  }
+function setupScrollPolyfill(element) {
+  var scrolling = false;
+  var node = element[0];
+  var lastScrollTime;
+  var lastScrollTop;
 
-  function loopScrollEvent() {
-    timestamp = new Date();
-    console.log(timestamp.getTime() - 1414700000000, scrollParent.scrollTop);
-    if(lastScrollPos == scrollParent.scrollTop){
-      console.log('stopping');
-      isScrolling = false;
-      ionic.trigger('$scrollend');
-    }else{
-      ionic.trigger('$scroll',{target:element}, eventBubbles);
-      //console.log('keep on scrolling');
-      lastScrollPos = scrollParent.scrollTop;
-      debounceScrollEvents();
+  element.on('touchmove scroll', onScroll);
+  function onScroll() {
+    lastScrollTime = +now();
+    if (!scrolling) {
+
+
+      element.triggerHandler({type:'$scrollstart', scrollTop:element.scrollTop});
+      scrolling = true;
+      scrollLoop();
     }
   }
 
-  ionic.on('touchend', function(){console.log('TOUCHEND')}, element);
+  function scrollLoop() {
+    var time = +now();
+    var scrollTop = node.scrollTop;
+    element.triggerHandler({type:'$scroll', scrollTop:element.scrollTop});
+    if (node.scrollTop !== lastScrollTop || time - lastScrollTime < 100) {
+      lastScrollTop = scrollTop;
+      ionic.requestAnimationFrame(scrollLoop);
+    } else {
+      element.triggerHandler({type:'$scrollend', scrollTop:element.scrollTop});
+      scrolling = false;
+    }
+  }
+
 }
 
