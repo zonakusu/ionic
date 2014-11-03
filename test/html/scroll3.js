4f17d0a63f9e955a0ce04c01bf7626584d25cc54
Perry Govier <govierp@gmail.com>
2014-11-03 13:29:13 -0600

    it's working! (still needs lots of love)

--- test/html/scroll3.js
+++ test/html/scroll3.js
@@ -4,20 +4,35 @@
 var now = window.performance ?
   angular.bind(window.performance, window.performance.now) :
   Date.now;
-
 function setupScrollPolyfill(element) {
   var scrolling = false;
   var node = element[0];
-  var lastScrollTime;
-  var lastScrollTop;
+  var self = this;
+  self.amplitude = 0;
+  self.frame = 0;
+  self.velocity = 0;
+  self.lastScrollTime = null;
+  self.lastScrollTop = null;
+  self.timestamp = null;
+  self.virtualScrollEventsRequired = false;
+  self.timeConstant = 325; // magic number
 
   element.on('touchmove scroll', onScroll);
+
+  if(ionic.Platform.isIOS()){
+    if(ionic.Platform.version() < 8 || ionic.Platform.isWebView()){
+      self.virtualScrollEventsRequired = true;
+      element.on('touchend', scrollPolyfill);
+      console.log('binding scrollPolyfill');
+    }
+  }
+
   function onScroll() {
-    lastScrollTime = +now();
+    self.lastScrollTime = +now();
     if (!scrolling) {
 
 
-      element.triggerHandler({type:'$scrollstart', scrollTop:element.scrollTop});
+      element.triggerHandler({type:'$scrollstart', scrollTop:node.scrollTop});
       scrolling = true;
       scrollLoop();
     }
@@ -26,13 +41,53 @@ function setupScrollPolyfill(element) {
   function scrollLoop() {
     var time = +now();
     var scrollTop = node.scrollTop;
-    element.triggerHandler({type:'$scroll', scrollTop:element.scrollTop});
-    if (node.scrollTop !== lastScrollTop || time - lastScrollTime < 100) {
-      lastScrollTop = scrollTop;
+
+    if(self.virtualScrollEventsRequired){
+      if(!self.velocity)self.velocity = 0;
+      var elapsed, delta, v;
+
+      elapsed = time - self.timestamp;
+      self.timestamp = time;
+      delta = scrollTop - self.frame;
+      self.frame = scrollTop;
+
+      v = 1000 * delta / (1 + elapsed);
+      self.velocity = 0.8 * v + 0.2 * self.velocity;
+    }
+
+    element.triggerHandler({type:'$scroll', scrollTop:node.scrollTop, element:element});
+    if (node.scrollTop !== self.lastScrollTop || time - self.lastScrollTime < 100) {
+      self.lastScrollTop = scrollTop;
       ionic.requestAnimationFrame(scrollLoop);
     } else {
-      element.triggerHandler({type:'$scrollend', scrollTop:element.scrollTop});
+      element.triggerHandler({type:'$scrollend', scrollTop:node.scrollTop});
       scrolling = false;
+    }
+  }
+
+  function scrollPolyfill() {
+    console.log(self.velocity, node.scrollTop, self.amplitude);
+    if (velocity > 10 || velocity < -10) {
+      self.amplitude = 0.8 * velocity;
+      self.target = Math.round(node.scrollTop + self.amplitude);
+      self.timestamp = Date.now();
+      requestAnimationFrame(scrollPolyfillLoop);
+    }
+  }
+
+
+  function scrollPolyfillLoop() {
+    var elapsed, delta;
+
+    if (!!self.amplitude) {
+      elapsed = Date.now() - self.timestamp;
+      delta = -self.amplitude * Math.exp(-elapsed / self.timeConstant);
+      if (delta > 0.5 || delta < -0.5) {
+        console.log(self.velocity, node.scrollTop, self.amplitude);
+        requestAnimationFrame(scrollPolyfillLoop);
+      } else {
+        console.log(target);
+      }
     }
   }
 
